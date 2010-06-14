4ff5dff1f04f4a0d272b55290b6183e82672eb98
yuoo2k <yuoo2k@57847e1f-48e1-6176-f44f-6a396297cb4d>
2010-06-14 13:28:26 +0000

    Initial sync to 1.5.20090525

---
+++ trunk/src/extension/defaults/preferences/ietab.js
@@ -0,0 +1,30 @@
+pref("ietab.filter", true);
+pref("ietab.filterlist", "/^file:\/\/\/.*\.(mht|mhtml)$/ http://*update.microsoft.com/* http://www.windowsupdate.com/*");
+
+pref("ietab.toolsmenu", true);
+pref("ietab.toolsmenu.icon", false);
+pref("ietab.statusbar", true);
+
+pref("ietab.handleUrlBar", false);
+pref("ietab.alwaysNewTab", false);
+pref("ietab.focustab", true);
+pref("ietab.handleBookmarkURL", true);
+
+pref("ietab.pagelink", true);
+pref("ietab.bookmark", true);
+pref("ietab.tabsmenu", true);
+
+pref("ietab.pagelink.extapp", true);
+pref("ietab.bookmark.extapp", true);
+pref("ietab.tabsmenu.extapp", true);
+
+pref("ietab.icon.pagelink", false);
+pref("ietab.icon.tabsmenu", false);
+pref("ietab.icon.bookmark", false);
+
+pref("ietab.ctrlclick", true);
+pref("ietab.extAppPath", "");
+pref("ietab.extAppParam", "%1");
+
+pref("ietab.titleEnding", "");
+pref("ietab.intl.charset.default", "");
