c8244791e9f0c7fbcf45bd59bc776204399c704d
pcman.tw@gmail.com <pcman.tw@gmail.com@57847e1f-48e1-6176-f44f-6a396297cb4d>
2012-11-18 15:11:09 +0000

    Update the packaging script to include GNU GPL license.txt in the package.

--- trunk/distribute/makepkg.bat
+++ trunk/distribute/makepkg.bat
@@ -1,8 +1,15 @@
 @echo off
-echo "Make package for IE Tab"
+echo Make package for IE Tab
+
 set /p ver=Please input version number:
-del *.xpi *.zip
-7z.exe a -mx9 -tzip -xr!.svn ietab-tmp.zip ..\src\extension\*
+
+del ietab-%ver%.xpi
+del ietab-tmp.zip
+
+7z.exe a -mx9 -tzip -xr!.svn ietab-tmp.zip ..\src\extension\* ..\src\plugin\license.txt
+
 rename ietab-tmp.zip ietab-%ver%.xpi
-echo "Finished!"
+
+echo Finished!
+
 pause
