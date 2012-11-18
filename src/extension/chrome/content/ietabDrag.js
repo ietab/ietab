de1a7ff1c5555a27c9a630547893486c2d221aec
pcman.tw@gmail.com <pcman.tw@gmail.com@57847e1f-48e1-6176-f44f-6a396297cb4d>
2012-11-18 13:45:25 +0000

    * Bump version number, preparing for a new release. * Add proper copyright information for javascript code to fullfill requirements of GPL. * Rename plugin folders. Move old deprecated XPCOM-based plugin to old_plugin.

--- src/extension/chrome/content/ietabDrag.js
+++ src/extension/chrome/content/ietabDrag.js
@@ -1,3 +1,22 @@
+//
+// ietabDrag.js
+//
+// Copyright (C) 2012 yuoo2k
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http:www.gnu.org/licenses/>.
+//
+
 // Status bar drag and drop functionality based off of example code from
 // http://kb.mozillazine.org/Dev_:_Extensions_:_Example_Code_:_Adding_Drag_and_Drop_to_Statusbarpanel
 //
